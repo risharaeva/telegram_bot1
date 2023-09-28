801a7136a290a3f8170e8b8a91fd6606ccb057e8
risharaeva <146364566+risharaeva@users.noreply.github.com>
2023-09-28 21:47:07 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from general_bot import run_whatstheholiday_bot
+from constants import TELEGRAM_TOKEN, OPENAI_TOKEN
+
+# запустить основного бота
+if __name__ == '__main__':
+    run_whatstheholiday_bot(TELEGRAM_TOKEN)
